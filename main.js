c429139145071d42b78765bf7119de4e67d2658c
1willcobb <cobb.will@gmail.com>
2023-07-16 13:05:30 -0700

    added a few things to main

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
 })
 
+
 renderer.setPixelRatio( window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.set(50,50,100);
